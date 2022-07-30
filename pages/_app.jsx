ab5e46e7931b5d76491e54575b62685a303493be
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-07-30 08:38:24 +0530

    :art: some extra codes removed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-w-min bg-black min-h-screen text-white py-4 select-none cursor-text">
+    <div className="">
       <Component {...pageProps} />
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Container from '@commons/components/atom/container';
 export default function Home() {
   return (
     <Container>
-      <span className="text-green-500">&gt;</span>
+      <span className="text-green-500">Hello World!</span>
     </Container>
   );
 }
